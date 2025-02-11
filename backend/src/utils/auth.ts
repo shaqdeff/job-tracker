97ac765c531a48b34ca09b3dee8f3b26a25c09ca
shaqdeff <shaquillendunda@gmail.com>
2025-02-12 00:55:00 +0300

    Update functions

--- backend/src/utils/auth.ts
+++ backend/src/utils/auth.ts
@@ -6,18 +6,18 @@ dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET ?? '';
 
-// hash a password
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10);
-  return bcrypt.hash(password, salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  return hashedPassword;
 };
 
-// compare password
 export const comparePassword = async (
   password: string,
   hashedPassword: string
 ): Promise<boolean> => {
-  return bcrypt.compare(password, hashedPassword);
+  const isMatch = await bcrypt.compare(password, hashedPassword);
+  return isMatch;
 };
 
 // generate a token
