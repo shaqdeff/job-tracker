05b92e07c04b275f40346659102b4a92fa305fcd
shaqdeff <shaquillendunda@gmail.com>
2025-02-12 00:54:26 +0300

    Fix dotenv config path

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,14 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import mongoose from 'mongoose';
 import authRoutes from './routes/auth.routes';
 
-dotenv.config();
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
 
 const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 5000;
+const port = process.env.PORT ? Number(process.env.PORT) : 8000;
 const MONGO_URI = process.env.MONGO_URI ?? '';
 
 const app = express();
@@ -16,19 +17,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// connect to database
+// connect to MongoDB
 mongoose
   .connect(MONGO_URI)
   .then(() => console.log('✅ MongoDB connected successfully!'))
   .catch((error) => console.error('❌ MongoDB connection failed:', error));
 
-// Routes
+// routes
 app.use('/auth', authRoutes);
 
 app.get('/', (req, res) => {
   res.send({ message: 'Server is running 🚀' });
 });
 
+// start the server
 app.listen(port, host, () => {
   console.log(`[ ready ] http://${host}:${port}`);
 });
