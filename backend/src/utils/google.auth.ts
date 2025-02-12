eaac253900a8af3c87d12282ac1153f28e57982a
shaqdeff <shaquillendunda@gmail.com>
2025-02-12 03:14:50 +0300

    Create google auth util

---
+++ backend/src/utils/google.auth.ts
@@ -0,0 +1,49 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+import path from 'path';
+import User from '../models/user.model';
+
+dotenv.config({ path: path.resolve(__dirname, '../../.env') });
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID ?? '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+      callbackURL: 'http://localhost:5000/auth/google/callback',
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = new User({
+            firstName: profile.name?.givenName ?? '',
+            lastName: profile.name?.familyName ?? '',
+            email: profile.emails?.[0].value ?? '',
+            googleId: profile.id,
+            isVerified: true,
+          });
+          await user.save();
+        }
+        done(null, user);
+      } catch (error) {
+        done(error, false);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user: any, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, null);
+  }
+});
